cf0e369e791073a5115e1832578fa159888b85d3
Abhiraj Singh <160131311+abhiraj-21@users.noreply.github.com>
2025-04-11 01:30:48 +0530

    Created #283.java

---
+++ Week-1/#283.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int[] result = new int[nums.length];
+        int index = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] != 0){
+                result[index] = nums[i];
+                index++;
+            }
+        }
+        for(int i=0; i<nums.length; i++){
+            nums[i] = result[i];
+        }
+    }
+}
+
