1f89c85fe0c7fe372fda06a39f8417ddf2b87638
Abhiraj Singh <160131311+abhiraj-21@users.noreply.github.com>
2025-04-17 23:03:52 +0530

    Create #189.java

---
+++ Week-2/#189.java
@@ -0,0 +1,19 @@
+//Reversing the entire array, then reversing the first k sub-array and then the remaining sub-array
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums,0,nums.length-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.length-1);
+    }
+    public void reverse(int[] arr, int startIndex, int endIndex){
+        while(startIndex <= endIndex){
+            int temp = arr[startIndex];
+            arr[startIndex] = arr[endIndex];
+            arr[endIndex] = temp;
+            startIndex++;
+            endIndex--;
+        }
+    }
+}
